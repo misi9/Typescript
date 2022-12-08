cc463e7738b46013f1e074acca47cfa5facee955
Misida Krakulli <misidakrakulli@gmail.com>
2022-12-08 01:35:59 +0100

    type assertions

---
+++ exercise-3-type-assertions-start.js
@@ -0,0 +1,15 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+function getPopulation() {
+    return 69950850;
+}
+// Add a type assertion after the call to `getPopulation()`.
+// Hint: What's the actual type of the value returned by `getPopulation()`?
+var population = getPopulation();
+// Now you've added a type assertion, there's a type error in the code below.
+// Remove the code that is incorrect.
+//console.log(population.toUpperCase());
+console.log(population * 2);
